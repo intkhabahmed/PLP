3485b2c65bd61d0a18d1c618bf816ac76163b9c8
Saptarshi Saha <saptarshi.saha@capgemini.com>
2017-11-08 14:49:41 +0530

    buddy i am here

--- src/com/cg/as/entity/BookingInformation.java
+++ src/com/cg/as/entity/BookingInformation.java
@@ -6,6 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name="bookinginformation")
@@ -19,27 +25,37 @@ public class BookingInformation {
 	private String custEmail;
 	
 	@Column(name="no_of_passengers")
+	@NotEmpty(message ="provide passengers seat")
 	private int noOfPassengers;
 	
+	
 	@Column(name="class_type")
+	@NotEmpty(message = "Provide a class")
 	private String classType;
 	
 	@Column(name="total_fare")
 	private double totalFare;
 	
 	@Column(name="CreditCard_info")
+	@NotEmpty(message = "Provide your credit card detail")
 	private String creditcardInfo;
 	
 	@Column(name="src_city")
+	@Pattern(regexp="[A-Za-z]{2,10}",message="Please provide valid source city")
+	@Size(min=2,max=10,message="size of the source between 2 to 10")
 	private String srcCity;
 	
 	@Column(name="dest_city")
+	@Pattern(regexp="[A-Za-z]{2,10}",message="Please provide valid destination city")
+	@Size(min=2,max=10,message="size of the destination city between 2 to 10")
 	private String destCity;
 	
 	@Column(name="flightNo")
 	private String flightNo;
 
 	@Column(name="booking_date")
+	@NotNull(message="Provide a date please")
+	@Future
 	private Date bookingDate;
 
 	
