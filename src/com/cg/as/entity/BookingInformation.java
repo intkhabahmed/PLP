e93bdd216f8b9aa0a247b9e0e009da8a4a314afd
Sinha <himanshu.a.sinha@capgemini.com>
2017-11-10 12:35:55 +0530

    by him

--- src/com/cg/as/entity/BookingInformation.java
+++ src/com/cg/as/entity/BookingInformation.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Future;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
